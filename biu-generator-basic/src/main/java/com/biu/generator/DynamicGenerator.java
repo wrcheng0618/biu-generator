a78c21216f13b39d7355d976da597e7aa4d2f4dd
王若丞 <wrcheng_0618@126.com>
2024-11-28 22:47:34 +0800

    生成动态文件工具类方法抽取

--- biu-generator-basic/src/main/java/com/biu/generator/DynamicGenerator.java
+++ biu-generator-basic/src/main/java/com/biu/generator/DynamicGenerator.java
@@ -17,17 +17,13 @@ import java.io.Writer;
  */
 public class DynamicGenerator {
 
-    public static void main(String[] args) throws IOException, TemplateException {
+    private static void doGenerate(String inputPath, String outputPath, Object model) throws IOException, TemplateException {
         // 新建（new）出 Configuration 对象，参数为 FreeMarker 版本号
         Configuration configuration = new Configuration(Configuration.VERSION_2_3_32);
 
-        // 指定模板文件所在的路径（输出：当前工作目录：D:\idea\biu-generator）
-        String projectPath = System.getProperty("user.dir");
-        System.out.println("当前工作目录：" + projectPath);
-        File projectFile = new File(projectPath);
-        // 由于工作目录是D:\idea\biu-generator，因此这么写会报FileNotFoundException
-        // configuration.setDirectoryForTemplateLoading(new File("src/main/resources/templates"));
-        configuration.setDirectoryForTemplateLoading(new File(projectFile, "biu-generator-basic/src/main/resources/templates"));
+        // 指定模板文件所在路径
+        File inputFile = new File(inputPath);
+        configuration.setDirectoryForTemplateLoading(inputFile.getParentFile());
 
         // 指定模板文件使用的字符集
         configuration.setDefaultEncoding("UTF-8");
@@ -36,19 +32,32 @@ public class DynamicGenerator {
         configuration.setNumberFormat("0.######");
 
         // 创建模板对象，加载指定模板
-        Template template = configuration.getTemplate("MainTemplate.java.ftl");
+        Template template = configuration.getTemplate(inputFile.getName());
+
+        // 生成
+        Writer out = new FileWriter(outputPath);
+        template.process(model, out);
+
+        // 生成文件后别忘了关闭哦
+        out.close();
+    }
+
+    public static void main(String[] args) throws IOException, TemplateException {
+        // 指定模板文件所在的路径（输出：当前工作目录：D:\idea\biu-generator）
+        String projectPath = System.getProperty("user.dir");
+        System.out.println("当前工作目录：" + projectPath);
+        // 由于工作目录是D:\idea\biu-generator，因此这么写会报FileNotFoundException
+        // String inputPath = projectPath + File.separator + "src/main/resources/templates/MainTemplate.java.ftl";
+        String inputPath = projectPath + File.separator + "biu-generator-basic/src/main/resources/templates/MainTemplate.java.ftl";
+
+        String outputPath = projectPath + File.separator + "biu-generator-basic/MainTemplate.java";
 
         // 创建数据模型
         MainTemplateConfig mainTemplateConfig = new MainTemplateConfig();
         mainTemplateConfig.setAuthor("biu");
-        mainTemplateConfig.setLoop(false);
+        mainTemplateConfig.setLoop(true);
         mainTemplateConfig.setOutputText("求和结果：");
 
-        // 生成
-        Writer out = new FileWriter(new File(projectFile, "biu-generator-basic/MainTemplate.java"));
-        template.process(mainTemplateConfig, out);
-
-        // 生成文件后别忘了关闭哦
-        out.close();
+        doGenerate(inputPath, outputPath, mainTemplateConfig);
     }
 }
